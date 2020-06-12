c9d7621510f146cd792e3d4d425b1cc4ccc44426
Amirhosein <gooyacoder@gmail.com>
2020-06-12 17:13:55 +0430

    Web API For Air Quality Index Configured

--- aqi/views.py
+++ aqi/views.py
@@ -2,4 +2,23 @@ from django.shortcuts import render
 
 
 def home(request):
-	return render(request, 'home.html', {})
+	import requests
+	import json
+
+	error = False
+
+	try:
+		request_api = requests.get('http://api.waqi.info/feed/shiraz/?token=e6089546a3dc14e1fe3d717777e9c7df3e7e5f99')
+		api = json.loads(request_api.content)
+	except Exception as e:
+		api = e
+		error = True
+
+
+
+
+	return render(request, 'home.html', {'api' : api, 'error' : error,})
+
+
+def about(request):
+	return render(request, 'about.html', {})
