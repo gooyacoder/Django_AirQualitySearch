a7d1829a111c3c6e8757936781380d99748c1aa0
Amirhosein <gooyacoder@gmail.com>
2020-06-14 16:21:01 +0430

    Search by city Added

--- aqi/views.py
+++ aqi/views.py
@@ -5,19 +5,77 @@ def home(request):
 	import requests
 	import json
 
-	error = False
+	if request.method == 'POST':
+		city = request.POST['city']
+		error = False
+		status = ''
+		status_color = ''
+		try:
+			request_api = requests.get('http://api.waqi.info/feed/' + city + '/?token=e6089546a3dc14e1fe3d717777e9c7df3e7e5f99')
+			api = json.loads(request_api.content)
+			if api['data']['aqi'] < 51:
+				status = 'Good'
+				status_color = 'good'
+			elif api['data']['aqi'] < 101:
+				status = 'Moderate'
+				status_color = 'moderate'
+			elif api['data']['aqi'] < 151:
+				status = 'Unhealthy for Sensitive Groups'
+				status_color = 'usg'
+			elif api['data']['aqi'] < 201:
+				status = 'Unhealthy'
+				status_color = 'unhealthy'
+			elif api['data']['aqi'] < 301:
+				status = 'Very Unhealthy'
+				status_color = 'veryunhealty'
+			else:
+				status = 'Hazardous'
+				status_color = 'hazardous'
+		except Exception as e:
+			er_msg_1 = 'No data found for the city "' + city + '".'
+			er_msg_2 = 'Please try again.'
+			error = True
+			return render(request, 'home.html', {'api' : api, 'error' : error,
+				'status' : status, 'status_color' : status_color, 'er_msg_1' : er_msg_1,
+				'er_msg_2' : er_msg_2,})
 
-	try:
-		request_api = requests.get('http://api.waqi.info/feed/shiraz/?token=e6089546a3dc14e1fe3d717777e9c7df3e7e5f99')
-		api = json.loads(request_api.content)
-	except Exception as e:
-		api = e
-		error = True
+
+		return render(request, 'home.html', {'api' : api, 'error' : error,
+		'status' : status, 'status_color' : status_color, })
+
+	else:
+
+		error = False
+
+		try:
+			request_api = requests.get('http://api.waqi.info/feed/shiraz/?token=e6089546a3dc14e1fe3d717777e9c7df3e7e5f99')
+			api = json.loads(request_api.content)
+		except Exception as e:
+			api = e
+			error = True
 
 
+		if api['data']['aqi'] < 51:
+			status = 'Good'
+			status_color = 'good'
+		elif api['data']['aqi'] < 101:
+			status = 'Moderate'
+			status_color = 'moderate'
+		elif api['data']['aqi'] < 151:
+			status = 'Unhealthy for Sensitive Groups'
+			status_color = 'usg'
+		elif api['data']['aqi'] < 201:
+			status = 'Unhealthy'
+			status_color = 'unhealthy'
+		elif api['data']['aqi'] < 301:
+			status = 'Very Unhealthy'
+			status_color = 'veryunhealty'
+		else:
+			status = 'Hazardous'
+			status_color = 'hazardous'
 
-
-	return render(request, 'home.html', {'api' : api, 'error' : error,})
+		return render(request, 'home.html', {'api' : api, 'error' : error,
+			'status' : status, 'status_color' : status_color,})
 
 
 def about(request):
