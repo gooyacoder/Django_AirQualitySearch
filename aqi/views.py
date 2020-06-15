48477b724d58208dd24d01b6b2db125c8664c180
Amirhosein <gooyacoder@gmail.com>
2020-06-15 17:23:17 +0430

    AQI Descriptions added

--- aqi/views.py
+++ aqi/views.py
@@ -16,21 +16,27 @@ def home(request):
 			if api['data']['aqi'] < 51:
 				status = 'Good'
 				status_color = 'good'
+				description = 'Air quality is satisfactory, and air pollution poses little or no risk.'
 			elif api['data']['aqi'] < 101:
 				status = 'Moderate'
 				status_color = 'moderate'
+				description = 'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'
 			elif api['data']['aqi'] < 151:
 				status = 'Unhealthy for Sensitive Groups'
 				status_color = 'usg'
+				description = 'Members of sensitive groups may experience health effects. The general public is less likely to be affected.'
 			elif api['data']['aqi'] < 201:
 				status = 'Unhealthy'
 				status_color = 'unhealthy'
+				description = 'Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.'
 			elif api['data']['aqi'] < 301:
 				status = 'Very Unhealthy'
 				status_color = 'veryunhealty'
+				description = 'Health alert: The risk of health effects is increased for everyone.'
 			else:
 				status = 'Hazardous'
 				status_color = 'hazardous'
+				description = 'Health warning of emergency conditions: everyone is more likely to be affected.'
 		except Exception as e:
 			er_msg_1 = 'No data found for the city "' + city + '".'
 			er_msg_2 = 'Please try again.'
@@ -41,7 +47,7 @@ def home(request):
 
 
 		return render(request, 'home.html', {'api' : api, 'error' : error,
-		'status' : status, 'status_color' : status_color, })
+		'status' : status, 'status_color' : status_color, 'description' : description, })
 
 	else:
 
@@ -58,24 +64,30 @@ def home(request):
 		if api['data']['aqi'] < 51:
 			status = 'Good'
 			status_color = 'good'
+			description = 'Air quality is satisfactory, and air pollution poses little or no risk.'
 		elif api['data']['aqi'] < 101:
 			status = 'Moderate'
 			status_color = 'moderate'
+			description = 'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'
 		elif api['data']['aqi'] < 151:
 			status = 'Unhealthy for Sensitive Groups'
 			status_color = 'usg'
+			description = 'Members of sensitive groups may experience health effects. The general public is less likely to be affected.'
 		elif api['data']['aqi'] < 201:
 			status = 'Unhealthy'
 			status_color = 'unhealthy'
+			description = 'Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.'
 		elif api['data']['aqi'] < 301:
 			status = 'Very Unhealthy'
 			status_color = 'veryunhealty'
+			description = 'Health alert: The risk of health effects is increased for everyone.'
 		else:
 			status = 'Hazardous'
 			status_color = 'hazardous'
+			description = 'Health warning of emergency conditions: everyone is more likely to be affected.'
 
 		return render(request, 'home.html', {'api' : api, 'error' : error,
-			'status' : status, 'status_color' : status_color,})
+			'status' : status, 'status_color' : status_color, 'description' : description, })
 
 
 def about(request):
