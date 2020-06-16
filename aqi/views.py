59363b36174ccde72b68275bb34702d1c4c24f2f
Amirhosein <gooyacoder@gmail.com>
2020-06-16 17:54:14 +0430

    Translated Description to Farsi

--- aqi/views.py
+++ aqi/views.py
@@ -16,27 +16,27 @@ def home(request):
 			if api['data']['aqi'] < 51:
 				status = 'Good'
 				status_color = 'good'
-				description = 'Air quality is satisfactory, and air pollution poses little or no risk.'
+				description = 'كیفیت هوا رضایت بخش است و آلودگی هوا خطری كم یا خطر ندارد.'
 			elif api['data']['aqi'] < 101:
 				status = 'Moderate'
 				status_color = 'moderate'
-				description = 'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'
+				description = 'کیفیت هوا قابل قبول است. با این حال ، ممکن است برای برخی از افراد به ویژه افرادی که به طور غیرمعمول نسبت به آلودگی هوا حساس هستند ، خطر ایجاد کند.'
 			elif api['data']['aqi'] < 151:
 				status = 'Unhealthy for Sensitive Groups'
 				status_color = 'usg'
-				description = 'Members of sensitive groups may experience health effects. The general public is less likely to be affected.'
+				description = 'ممکن است بر سلامتی اعضای گروههای حساس اثر بگذارد. عموم مردم کمتر تحت تأثیر قرار می گیرند.'
 			elif api['data']['aqi'] < 201:
 				status = 'Unhealthy'
 				status_color = 'unhealthy'
-				description = 'Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.'
+				description = 'برخی از افراد عمومی ممکن است اثرات آلودگی هوا را بر سلامتی خود تجربه کنند. اعضای گروههای حساس ممکن است اثرات جدی تری را تجربه کنند.'
 			elif api['data']['aqi'] < 301:
 				status = 'Very Unhealthy'
 				status_color = 'veryunhealty'
-				description = 'Health alert: The risk of health effects is increased for everyone.'
+				description = 'هشدار سلامتی: خطر تأثیرات آلودگی هوا بر سلامتی، برای همه افزایش یافته است.'
 			else:
 				status = 'Hazardous'
 				status_color = 'hazardous'
-				description = 'Health warning of emergency conditions: everyone is more likely to be affected.'
+				description = 'هشدار بهداشتی در مورد شرایط اضطراری: همه به احتمال زیاد تحت تأثیر قرار می گیرند.'
 		except Exception as e:
 			er_msg_1 = 'No data found for the city "' + city + '".'
 			er_msg_2 = 'Please try again.'
@@ -64,27 +64,27 @@ def home(request):
 		if api['data']['aqi'] < 51:
 			status = 'Good'
 			status_color = 'good'
-			description = 'Air quality is satisfactory, and air pollution poses little or no risk.'
+			description = 'كیفیت هوا رضایت بخش است و آلودگی هوا خطری كم یا خطر ندارد.'
 		elif api['data']['aqi'] < 101:
 			status = 'Moderate'
 			status_color = 'moderate'
-			description = 'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'
+			description = 'کیفیت هوا قابل قبول است. با این حال ، ممکن است برای برخی از افراد به ویژه افرادی که به طور غیرمعمول نسبت به آلودگی هوا حساس هستند ، خطر ایجاد کند.'
 		elif api['data']['aqi'] < 151:
 			status = 'Unhealthy for Sensitive Groups'
 			status_color = 'usg'
-			description = 'Members of sensitive groups may experience health effects. The general public is less likely to be affected.'
+			description = 'ممکن است بر سلامتی اعضای گروههای حساس اثر بگذارد. عموم مردم کمتر تحت تأثیر قرار می گیرند.'
 		elif api['data']['aqi'] < 201:
 			status = 'Unhealthy'
 			status_color = 'unhealthy'
-			description = 'Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.'
+			description = 'برخی از افراد عمومی ممکن است اثرات آلودگی هوا را بر سلامتی خود تجربه کنند. اعضای گروههای حساس ممکن است اثرات جدی تری را تجربه کنند.'
 		elif api['data']['aqi'] < 301:
 			status = 'Very Unhealthy'
 			status_color = 'veryunhealty'
-			description = 'Health alert: The risk of health effects is increased for everyone.'
+			description = 'هشدار سلامتی: خطر تأثیرات آلودگی هوا بر سلامتی، برای همه افزایش یافته است.'
 		else:
 			status = 'Hazardous'
 			status_color = 'hazardous'
-			description = 'Health warning of emergency conditions: everyone is more likely to be affected.'
+			description = 'هشدار بهداشتی در مورد شرایط اضطراری: همه به احتمال زیاد تحت تأثیر قرار می گیرند.'
 
 		return render(request, 'home.html', {'api' : api, 'error' : error,
 			'status' : status, 'status_color' : status_color, 'description' : description, })
